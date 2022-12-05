ae51133d768d04f8d853d61d6ffd733ab0650b6e
Jordan <Jordantay9014@outlook.com>
2022-12-05 14:45:08 -0600

    added autnetication

---
+++ Conduit.API/Models/UserDto.cs
@@ -0,0 +1,8 @@
+namespace Conduit.API.Models;
+
+public class UserDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
